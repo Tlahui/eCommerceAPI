d72091f68914a3eb119a665d15dd2b85c9594094
Roberto Novelo <robertod.novelo@gmail.com>
2015-12-19 13:37:07 -0600

    Conekta Card Charge

--- application/controllers/user/payment.php
+++ application/controllers/user/payment.php
@@ -1,10 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require_once(BASEPATH . '../application/libraries/Conekta.php');
+
 class Payment extends CI_Controller {
 
 	public function checkout()
 	{
-		echo "helloworld";
+		Conekta::setApiKey('key_Fq5U8GUU28hTqgxy4md4TQ');
+		try
+		{
+			$charge = Conekta_Charge::create(array(
+				"amount"=> 51000,
+				"currency"=> "MXN",
+				"description"=> "Tlahui",
+				"reference_id"=> "Tlahui100",
+				"card"=> $this->input->post("token")
+			));
+			var_dump($charge);
+		}
+		catch (Conekta_Error $e)
+		{
+			echo $e->getMessage();
+		}
 	}
 }
 

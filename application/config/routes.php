a490683d5dea11f5d016a58422f1b2a206c5aea2
felimargom <kastulink@hotmail.com>
2015-12-19 14:56:40 -0600

    subscribe

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,8 @@
 $route['user/upload'] =  "user/upload/upload_image";
 $route['user/checkout'] =  "user/payment/checkout";
 $route['user/add/card'] =  "user/payment/register_card";
+
+$route['user/plan/subscribe'] =  "user/payment/subscribe";
 $route['conekta/user/webhook'] =  "user/payment/confirm_payment";
 
 

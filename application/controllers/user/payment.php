29b0e41172a07671514b591b74d9241065fbc3dd
Roberto Novelo <robertod.novelo@gmail.com>
2015-12-19 13:22:43 -0600

    User Charge Route, Conekta Lib Files

---
+++ application/controllers/user/payment.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Payment extends CI_Controller {
+
+	public function checkout()
+	{
+		echo "helloworld";
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */

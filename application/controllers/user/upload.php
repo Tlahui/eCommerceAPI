e52604a3649e86a5e6a44d6d7d6d6d5ced3bd697
Galileo Guzmán <galileoguzman@gmail.com>
2015-12-19 09:01:39 -0600

    create new controller to manage upload image

---
+++ application/controllers/user/upload.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Upload extends CI_Controller {
+	public function upload_image()
+	{
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/user/upload.php */

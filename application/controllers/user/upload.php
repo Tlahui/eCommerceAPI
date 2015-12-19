e44195485bce57de0f4dd0f6623ae0f14e995f7f
Roberto Novelo <robertod.novelo@gmail.com>
2015-12-19 09:01:17 -0600

    User Upload Route + Empty Controller

---
+++ application/controllers/user/upload.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Upload extends CI_Controller {
+
+	public function upload_image()
+	{
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
